abd940f269db1f0297c5931bbf2db40beed0d433
Lưu Tuấn Anh <139638161+tuananh190@users.noreply.github.com>
2025-08-09 23:54:32 +0700

    chuỗi trên intell

---
+++ arr.java
@@ -0,0 +1,13 @@
+public class arr {
+    public static void main(String[]args){
+        int[]arr1 = new int[8];
+        arr1[0] = {7};
+
+        int [] arr = {1,2,3,4,5,6};
+        int i = 3;
+        for ( ; i< arr.length ; i++){
+            System.out.println(arr[i]);
+
+        }
+    }
+}
