1ec591feb96e1a39c6e2255fc11cc3c62d389f05
Lưu Tuấn Anh <139638161+tuananh190@users.noreply.github.com>
2025-08-11 15:30:22 +0700

    for simple

---
+++ arr2.java
@@ -0,0 +1,8 @@
+public class arr2 {
+    public static void main(String[]args){
+        int [] arr2 = {1,2,3,4,5,6,7,8,9,10,11};
+        for (int a = 1 ; a>= 3; a++){
+            System.out.println(arr2[a]);
+        }
+    }
+}
